51143d8c322bf6d7f6fde5db13ac18fa832a0dce
Namanfalke <namanfalke2@gmail.com>
2026-08-11 08:48:55 +0530

    Refactor Streamlit app for customer prediction

--- streamlite/app.py
+++ streamlite/app.py
@@ -31,11 +31,12 @@ if st.button('Submit'):
     # st.write(input_data)
     # st.write(scaled_input)
     prediction = knn.predict(scaled_input)
+    if prediction == "Yes":
+        st.success("✅ Customer is likely to subscribe")
+    else:
+        st.error("❌ Customer is unlikely to subscribe")
 
-if prediction == "Yes":
-    st.success("✅ Customer is likely to subscribe")
-else:
-    st.error("❌ Customer is unlikely to subscribe")
+
 
 # if prediction == "yes":
 #     st.write("you are eligible for taking subscriptions of this bank ")
