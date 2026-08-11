3c9c1ce45b5b11c3ae8d35e77de2a8f716ce26c6
Namanfalke <namanfalke2@gmail.com>
2026-08-11 08:34:58 +0530

    Correct prediction condition syntax
    Fixed syntax errors in prediction conditionals.

--- streamlite/app.py
+++ streamlite/app.py
@@ -31,7 +31,7 @@ if st.button('Submit'):
     # st.write(input_data)
     # st.write(scaled_input)
     prediction = knn.predict(scaled_input)
-    if prediction = yes :
+    if prediction == yes :
         st.write(prediction, "you are eligible for taking subscriptions of this bank ")
-    elif prediction = no :
+    elif prediction == no :
         st.write(prediction, "you are not eligible for taking subscriptions of this bank ")
